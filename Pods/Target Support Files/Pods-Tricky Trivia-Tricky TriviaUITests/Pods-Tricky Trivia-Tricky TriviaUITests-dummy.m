19969f2fd5eb7fc632a2034bd50f1c756b88c920
Jeremy Meharg <jeremymm125@gmail.com>
2022-04-10 21:28:25 -0700

    Adding the start of the project

---
+++ Pods/Target Support Files/Pods-Tricky Trivia-Tricky TriviaUITests/Pods-Tricky Trivia-Tricky TriviaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tricky_Trivia_Tricky_TriviaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Tricky_Trivia_Tricky_TriviaUITests
+@end
---
+++ Tricky Trivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tricky Trivia
+//
+//  Created by Jeremy Meharg on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
