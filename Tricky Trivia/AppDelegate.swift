c410354ee7560901762d03c00333d0b1f7804d20
Jeremy Meharg <jeremymm125@gmail.com>
2022-04-12 20:06:58 -0700

    added parse to the backend

--- Tricky Trivia/AppDelegate.swift
+++ Tricky Trivia/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "cHCGp2I748L0sVDZFMSFTjepV2y3spU2au7GuJJS" // <- UPDATE
+                    $0.clientKey = "YNywMjVCFXxyBXwg1bmx1SUn6Tnl8WLpQMU0sopU" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
